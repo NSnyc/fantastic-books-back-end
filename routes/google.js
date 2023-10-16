a2bf403b936a1d25ce55355e8be74a1692f0bb9f
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-16 11:48:12 -0400

    create route and async function to get volumeId and info

--- routes/google.js
+++ routes/google.js
@@ -4,6 +4,6 @@ import * as googleCtrl from '../controllers/google.js'
 const router = Router()
 
 // GET localhost:3002
+router.get('/:volumeId', googleCtrl.getBookDetailsById)
 router.post('/', googleCtrl.bookSearch)
-
 export { router }
